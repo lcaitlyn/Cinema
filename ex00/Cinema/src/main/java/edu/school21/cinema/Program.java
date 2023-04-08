356d6b8c1fabca679d8803df304fb11c215438cc
lcaitlyn <mrrifatcool@gmail.com>
2023-04-08 14:18:39 +0300

    делаю добавление

--- ex00/Cinema/src/main/java/edu/school21/cinema/Program.java
+++
@@ -1,5 +0,0 @@
-package edu.school21.cinema;
-
-public class Program {
-    
-}
--- ex00/Cinema/src/main/java/edu/school21/cinema/controllers/HallController.java
+++ ex00/Cinema/src/main/java/edu/school21/cinema/controllers/HallController.java
@@ -1,11 +1,13 @@
 package edu.school21.cinema.controllers;
 
-import edu.school21.cinema.models.Hall;
-import edu.school21.cinema.repositories.HallRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,12 +23,15 @@ public class HallController {
     public String halls(Model model) {
         List<Hall> halls = hallRepository.findAll();
         model.addAttribute("halls", halls);
+        model.addAttribute("hall", new Hall());
         return "halls";
     }
 
-    @GetMapping("/add")
-    public String fuck() {
-        return "fuck";
+    @PostMapping("/add")
+    public String fuck(@ModelAttribute Hall hall, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) {
+            hallRepository.
+        }
     }
 
 
